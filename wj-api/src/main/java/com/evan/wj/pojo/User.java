18580cc6fc523ac2cecca8d52ebe59fc307aa655
li0228 <2482519180@qq.com>
2020-11-26 20:40:53 +0800

    配置跨域

---
+++ wj-api/src/main/java/com/evan/wj/pojo/User.java
@@ -0,0 +1,37 @@
+package com.evan.wj.pojo;
+
+/**
+ * 用户类
+ *
+ * @author lihonghao
+ * @date 2020/11/26 20:37
+ */
+public class User {
+	int id;
+	String username;
+	String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ wj-api/src/main/java/com/evan/wj/result/Result.java
@@ -0,0 +1,8 @@
+package com.evan.wj.result;
+
+/**
+ * @author lihonghao
+ * @date 2020/11/26 20:40
+ */
+public class Result {
+}
