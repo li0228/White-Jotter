07083e86295f5f92aa682988cb8a2f4967e9fe95
li0228 <2482519180@qq.com>
2020-11-26 21:36:47 +0800

    修改properties配置文件为yml配置文件

--- wj-api/src/main/java/com/evan/wj/WjApiApplication.java
+++ wj-api/src/main/java/com/evan/wj/WjApiApplication.java
@@ -2,8 +2,9 @@ package com.evan.wj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication public class WjApiApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) public class WjApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WjApiApplication.class, args);
--- wj-api/src/main/java/com/evan/wj/result/Result.java
+++ wj-api/src/main/java/com/evan/wj/result/Result.java
@@ -5,4 +5,18 @@ package com.evan.wj.result;
  * @date 2020/11/26 20:40
  */
 public class Result {
+	//响应码
+	private int code;
+
+	public Result(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
 }
