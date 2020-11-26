e4e030961e68b5a7d8a494d65374e04de65fd373
li0228 <2482519180@qq.com>
2020-11-26 20:42:57 +0800

    新建后端springBoot项目

---
+++ wj-api/src/main/java/com/evan/wj/WjApiApplication.java
@@ -0,0 +1,12 @@
+package com.evan.wj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class WjApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjApiApplication.class, args);
+	}
+
+}
--- wj-vue/src/router/index.js
+++ wj-vue/src/router/index.js
@@ -2,14 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+// 登录和主页
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+		{
+		  path: '/index',
+		  name: 'AppIndex',
+		  component: AppIndex
+		}
   ]
 })
